afc2ae6b1d758ca0c7f2fc933df06bdaecc3a7fe
Polash <polashtechai@gmail.com>
2025-07-13 11:47:42 +0600

    .gitignore and README.md added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Polash MSH',
+    author_email= 'polashtechai@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)

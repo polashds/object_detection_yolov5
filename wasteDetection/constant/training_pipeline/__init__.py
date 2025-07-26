eab20b94c5080b3152f99255e7600ebecb1adf3e
Polash <polashtechai@gmail.com>
2025-07-27 05:59:01 +0600

    boilerplate added

--- wasteDetection/constant/training_pipeline/__init__.py
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -24,11 +24,12 @@ DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
 
 
 """
-Data Validation realted contant start with DATA_VALIDATION VAR NAME
+MODEL TRAINER related constant start with MODEL_TRAINER var name
 """
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 
-DATA_VALIDATION_DIR_NAME: str = "data_validation"
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
 
-DATA_VALIDATION_STATUS_FILE = 'status.txt'
+MODEL_TRAINER_NO_EPOCHS: int = 1
 
-DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
+MODEL_TRAINER_BATCH_SIZE: int = 16
